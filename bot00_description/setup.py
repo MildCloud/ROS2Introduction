58f38d035f90a0f7d9d37f11da15a9334ca26c26
tian-shu-ASUS <tianshumild@gmail.com>
2023-08-25 13:52:10 -0400

    reset for wall

--- bot00_description/setup.py
+++ bot00_description/setup.py
@@ -14,8 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/**')),
-        (os.path.join('share', package_name, 'world/intro.world'), glob('world/intro.world/**'))
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/**'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
