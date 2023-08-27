d1190c08278a04e32829b8d7065b0e398caade5c
tian-shu-ASUS <tianshumild@gmail.com>
2023-08-26 22:36:44 -0400

    refomat bot description

---
+++ bot_description/install/bot00_description/share/bot00_description/launch/gazebo.launch.py
@@ -0,0 +1,43 @@
+import os
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_name_in_model = 'bot00'
+    package_name = 'bot00_description'
+    urdf_name = 'urdf_intro.urdf'
+
+    ld = LaunchDescription()
+    pkg_share = FindPackageShare(package=package_name).find(package_name=package_name)
+    urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    gazebo_world_path = os.path.join(pkg_share, 'world')
+
+    start_gazebo_cmd = ExecuteProcess(cmd=['gazebo', '--verbose','-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen')
+
+    spawn_entity_cmd = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-entity', robot_name_in_model,  '-file', urdf_model_path ], output='screen')
+    
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        arguments=[urdf_model_path]
+    )
+
+    start_rviz_cmd = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+    )
+    
+    ld.add_action(start_gazebo_cmd)
+    ld.add_action(spawn_entity_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(start_rviz_cmd)
+
+    return ld
