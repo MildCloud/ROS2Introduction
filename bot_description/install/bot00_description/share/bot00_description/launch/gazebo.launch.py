2120c554a9d8c685feaeb9203b131365b1732eae
tian-shu-ASUS <tianshumild@gmail.com>
2023-08-29 12:41:58 -0400

    nav2 set goal

--- bot_description/install/bot00_description/share/bot00_description/launch/gazebo.launch.py
+++ bot_description/install/bot00_description/share/bot00_description/launch/gazebo.launch.py
@@ -38,6 +38,6 @@ def generate_launch_description():
     ld.add_action(start_gazebo_cmd)
     ld.add_action(spawn_entity_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
-    ld.add_action(start_rviz_cmd)
+    # ld.add_action(start_rviz_cmd)
 
     return ld
